033e7a64b15f3916ba2bd3730be198d05ba2a81d
eekhin <eekhin@gmail.com>
2017-09-11 18:53:09 -0700

    Added package-info

---
+++ src/main/java/com/sqa/eiei/helpers/enums/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * File Name: package-info.java<br>
+ * KHIN, EI EI<br>
+ * Created: Sep 9, 2017
+ */
+/**
+ * package-info //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      KHIN, EI EI
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+package com.sqa.eiei.helpers.enums;
